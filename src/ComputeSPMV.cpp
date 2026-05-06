5503f9a0796329fe51829be46043cefa43cf8e66
amei <aretimei2233@gmail.com>
2026-05-06 08:55:21 +0200

    added src/ComputeSPMV.cpp

--- src/ComputeSPMV.cpp
+++ src/ComputeSPMV.cpp
@@ -20,6 +20,10 @@
 
 #include "ComputeSPMV.hpp"
 #include "ComputeSPMV_ref.hpp"
+#include <stdio.h>
+#include <riscv_vector.h>
+#include <omp.h>
+#include <assert.h>
 
 /*!
   Routine to compute sparse matrix vector product y = Ax where:
@@ -37,9 +41,49 @@
 
   @see ComputeSPMV_ref
 */
-int ComputeSPMV( const SparseMatrix & A, Vector & x, Vector & y) {
 
-  // This line and the next two lines should be removed and your version of ComputeSPMV should be used.
-  A.isSpmvOptimized = false;
-  return ComputeSPMV_ref(A, x, y);
+int ComputeSPMV(const SparseMatrix &A, Vector &x, Vector &y) {
+
+  A.isSpmvOptimized = true;
+  assert(x.localLength >= A.localNumberOfColumns);
+  assert(y.localLength >= A.localNumberOfRows);
+
+#ifndef HPCG_NO_MPI
+  ExchangeHalo(A, x, 0);
+#endif
+
+  const double * const xv = x.values;
+  double * const yv = y.values;
+  const local_int_t nrow = A.localNumberOfRows;
+  double sum;
+
+  for (local_int_t i=0; i<nrow; i++) {
+    sum = 0.0;
+    const double *cur_vals = A.matrixValues[i];
+    const local_int_t *cur_inds = A.mtxIndL[i];
+    const int cur_nnz = A.nonzerosInRow[i];
+
+    for (local_int_t colid=0; colid<cur_nnz;) {
+
+      size_t givenVectorLength = __riscv_vsetvl_e64m1(cur_nnz - colid);
+
+      vfloat64m1_t va = __riscv_vle64_v_f64m1(cur_vals + colid, givenVectorLength);
+      vfloat64m1_t v_x;
+
+      vuint64m1_t v_idx_col =__riscv_vle64_v_u64m1((const uint64_t *)(cur_inds + colid), givenVectorLength);
+
+      v_idx_col = __riscv_vsll_vx_u64m1(v_idx_col, 3, givenVectorLength);
+      v_x = __riscv_vluxei64_v_f64m1(xv, v_idx_col, givenVectorLength);
+      vfloat64m1_t vprod =__riscv_vfmul_vv_f64m1(va, v_x, givenVectorLength);
+      vfloat64m1_t partial_res =__riscv_vfmv_v_f_f64m1(0.0, givenVectorLength);
+      partial_res = __riscv_vfredosum_vs_f64m1_f64m1(vprod, partial_res, givenVectorLength);
+      sum += __riscv_vfmv_f_s_f64m1_f64(partial_res);
+      colid += givenVectorLength;
+    }
+
+    yv[i] = sum;
+
+  }
+
+  return 0;
 }
